682df9952c55bbd0fcfdd29e17d9d5050fa9b7a8
wjk1993 <wangjk9@yonyou.com>
2021-03-15 13:54:50 +0800

    vue cli 移动项目初始化2

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+注释-----
+reset.css  重置统一浏览器标准css基础样式
+border.css 解决2倍屏幕像素问题
+fastclick依赖  解决点击延迟300ms问题
+stylus stylus-loader 类似于less sass  注意版本过高的报错 hasopen
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont icon-zuojiantou back-icon"></span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont icon-sousuo"></span>
+      输入
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-jiantouxia arrow-icon"></span>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style  scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    height: .86rem
+    background: $bgColor
+    line-height: .86rem
+    color: #ffffff
+    .header-left
+      float: left
+      width: .64rem
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      margin-top: .12rem
+      margin-left: .2rem
+      height: .64rem
+      line-height: .64rem
+      background: #ffffff
+      color: #ccc
+      border-radius: .1rem
+      padding-left: .2rem
+    .header-right
+      float: right
+      width: 1.24rem
+      text-align center
+      .arrow-icon
+        margin-left :-.04rem
+        font-size: .1rem
+</style>
